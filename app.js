8ecd4d27b115d780e8d2c7b331ef81bad380d3cb
Abd1h <abdlh.lllh@gmail.com>
2022-12-14 21:14:13 +0300

    user functionality 50%

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const tours = JSON.parse(
 );
 
 //// ---------------functions-------------------
+//1) tours functions
 const getAllTours = function (req, res) {
   res.status(200).json({
     status: 'success',
@@ -50,7 +51,7 @@ const getSingleTour = function (req, res) {
   });
 };
 
-const createNewTour = function (req, res) {
+const createTour = function (req, res) {
   //1) getting post data
   const postData = req.body;
   //2) create an ID for the new tour
@@ -85,13 +86,84 @@ const deleteTour = function (req, res) {
   });
 };
 
+//2) user functions
+const getAllUsers = function (req, res) {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours: tours },
+  });
+};
+
+const getSingleUser = function (req, res) {
+  //1) getting the tour id
+  const id = +req.params.id;
+  //2) finding tour with that id
+  const tour = tours.find((tour) => tour.id === id);
+  //3) if tour exist then send it as a respond
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+    return;
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour: tour },
+  });
+};
+
+const createUser = function (req, res) {
+  //1) getting post data
+  const postData = req.body;
+  //2) create an ID for the new tour
+  const newTourId = tours.length;
+  //3) new object with data and newId
+  const newTour = Object.assign(postData, { id: newTourId });
+  //4) update the tours array
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      //201 means "updated"
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    }
+  );
+};
+
+const updateUser = function (req, res) {
+  res.status(200).json({
+    status: 'success',
+    message: 'tour was updated',
+  });
+};
+const deleteUser = function (req, res) {
+  res.status(200).json({
+    status: 'success',
+    data: 'null',
+  });
+};
+
 //// handling tour route
-app.route('/api/v1/tours').get(getAllTours).post(createNewTour);
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:id')
   .get(getSingleTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+//// handling user route
+app.route('/api/v1/tours').get(getAllUsers).post(createUser);
+app
+  .route('/api/v1/tours/:id')
+  .get(getSingleUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 const serverPort = 8000;
 app.listen(serverPort);
