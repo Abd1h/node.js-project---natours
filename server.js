4bd632aaf6059ab1c250ffe6fb7210d278d8f0f1
Abd1h <abdlh.lllh@gmail.com>
2023-02-06 11:29:20 +0300

    setting mongoose

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose'); //library for mongoDB, a higher level of abstraction
+
 const dotenv = require('dotenv'); //allow us to create environment variables using config file
 
 dotenv.config({ path: './config.env' }); //must be before require app
@@ -5,5 +7,19 @@ dotenv.config({ path: './config.env' }); //must be before require app
 const app = require('./app');
 
 //<><><><><><><><><><><><><><><><><><><><><><><><><><>//
+// getting our DB connection link after setting the mongoDB password
+const DBlink = process.env.DATABASE.replace(
+  '<DATABASE_PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+//connecting with our database using mongoose
+mongoose
+  .connect(DBlink, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }) //NOTE mongoose connect methods return a promise with object
+  .then(() => console.log('mongoose connected'));
+
 const serverPort = 8000;
-app.listen(serverPort);
+app.listen(serverPort, () => console.log(`app running on port ${serverPort}`));
