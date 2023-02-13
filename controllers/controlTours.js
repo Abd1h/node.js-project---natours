89dab681fd73ed433067cb53cdb99691bb4d7320
Abd1h <abdlh.lllh@gmail.com>
2023-02-13 15:10:50 +0300

    search functionality class - done

--- controllers/controlTours.js
+++ controllers/controlTours.js
@@ -1,5 +1,5 @@
 const Tour = require('../models/tourModels');
-
+const SearchFeatures = require('../utils/searchFeatures');
 //-------------------------------------------
 //         middleware
 //-------------------------------------------
@@ -12,65 +12,21 @@ exports.top5Cheap = function (req, res, next) {
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
 };
+
 //-------------------------------------------
 //       route methods functionality
 //-------------------------------------------
 exports.getAllTours = async function (req, res) {
   try {
-    // 1) FILTERING url using the query string
-    //note const queryObj = req.query; --> 🛑WRONG
-    const queryObj = { ...req.query };
-    console.log(queryObj);
-    const excludedFields = ['page', 'sort', 'limit', 'fields']; //not for searching keys
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // * advance FILTERING
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(
-      /\b(gte|gt|lte|lt)\b/g, //b = this exact word,g = everywhere
-      (match) => `$${match}` //note: replace return a callback
-    );
-    //note we dont await so we can use other methods 'sort,skip..'
-    let query = Tour.find(JSON.parse(queryString));
-
-    // 2) SORTING
-    //127.0.0.1:8000/api/v1/tours?name=aaa&sort=-price,duration
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      //note that sort return a query so you can chain other methods
-      query = query.sort(sortBy);
-    } else {
-      //defualt --> sort by date
-      query = query.sort('_createdAt');
-    }
-
-    // 3) FIELD LIMITING 'not sending all keys of the data object'
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      // remove fields that are created by mongoose like (__v) using (-)
-      query.select('-__v');
-    }
-
-    // 4) PAGINATION '/api/v1/tours? page=2 & limit=5'
-
-    // page.1 => 1-5 // page.2 => 6-10 // page.3 => 11-15
-    // methods => skip() ,limit()
-    const page = +req.query.page || 1;
-    const limit = +req.query.limit || 100;
-    const skipNum = (page - 1) * limit;
-
-    query = query.skip(skipNum).limit(limit);
-    //if user skips all tours
-    if (req.query.page) {
-      const numberOfTours = await Tour.countDocuments();
-      if (skipNum >= numberOfTours) throw new Error('This Page Dose Not Exist');
-    }
-    // * EXECUTE QUERY
-    const tours = await query;
-    //note: if there is query string -->{name = The Sea}
-    //and   if there isnt -->{empty} which will select all tours
+    //1) getting result form DB after applying our search features
+    const searchQuery = new SearchFeatures(req.query, Tour.find())
+      .filtering()
+      .sorting()
+      .fieldsLimiting()
+      .pagination();
+    // 2) await for query result
+    const tours = await searchQuery.queryResults;
+    // 3) send back resurlt as a respond
     res.status(200).json({
       status: 'success',
       results: tours.length,
