105cb1a7413af8b9185c2e048a57f2e5efa1419d
Abd1h <abdlh.lllh@gmail.com>
2023-03-14 18:02:48 +0300

    applying virtual properties

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,59 +1,76 @@
 const mongoose = require('mongoose');
 
 // creating mongoose schema for tours
-const tourSchema = new mongoose.Schema({
-  // setting key with only type or object of options
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: [true, 'use different name'],
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    // setting key with only type or object of options
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: [true, 'use different name'],
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'a tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'a tour must have a max group size'],
+    },
+    difficulty: {
+      type: String,
+      required: true,
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    reatingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'a tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true, // this will trim all the extra spaces
+      required: [true, 'a tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String, //cuz we will put the path of the img
+      required: [true, 'a tour must have a cover img'],
+    },
+    createdAt: {
+      type: Date, //NEW
+      default: Date.now(), //will give time in ms, and mongoose will convert it to a date
+    },
+    startDates: [Date], // array of dates
+    images: [String],
   },
-  duration: {
-    type: Number,
-    required: [true, 'a tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'a tour must have a max group size'],
-  },
-  difficulty: {
-    type: String,
-    required: true,
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  reatingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'a tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true, // this will trim all the extra spaces
-    required: [true, 'a tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String, //cuz we will put the path of the img
-    required: [true, 'a tour must have a cover img'],
-  },
-  createdAt: {
-    type: Date, //NEW
-    default: Date.now(), //will give time in ms, and mongoose will convert it to a date
-  },
-  startDates: [Date], // array of dates
-  images: [String],
+  //options object
+  {
+    //virtual properties settings
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+//virtual properties
+tourSchema.virtual('durationWeek').get(function () {
+  //1) must return the proprite value
+  //2) this prop will be created on every get call
+  //3) ()=> has issues with the 'this keyword'
+  return this.duration / 7;
 });
+
 //creating module of that schema
 //NOTE model name start with upper case letter
 //NOTE the name of the collection will come from 'Tour'
