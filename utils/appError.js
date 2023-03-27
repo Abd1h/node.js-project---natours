fc5c5bdee466598c2616f41d7a2a5091bf7d3345
Abd1h <abdlh.lllh@gmail.com>
2023-03-27 18:41:59 +0300

    implementing AppError class

---
+++ utils/appError.js
@@ -0,0 +1,16 @@
+class appError extends Error {
+  constructor(message, statusCode) {
+    //super calls the Error build in class which only takes message
+    //also the Error class will set the message proprty to message value
+    super(message);
+
+    this.statuscode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'; //404 =fail , 500 = error
+    this.operationalError = true;
+
+    //below is to not add this error to the error stack*
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = appError;
